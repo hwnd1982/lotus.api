1d53c9c5db3a3afb698c54e4d305851e8fc5f967
Kirill Lavrov <lkv@myttk.ru>
2024-11-24 02:26:50 +0700

    fixed registration .02

--- index.ts
+++ index.ts
@@ -24,14 +24,16 @@ app.get("/*", (req, res) => {
     return res.sendFile(join(__dirname, env.APP_URL, req.url));
   }
 
-  if (/\/registration/.test(req.url)) {
-    const db = JSON.parse(readFileSync(join(__dirname, env.DB_URL, "db.json"), "utf8"));
-    const url = parse(req.url);
+  const db = JSON.parse(readFileSync(join(__dirname, env.DB_URL, "db.json"), "utf8"));
+  const url = parse(req.url);
 
-    io.once("connection", socket => {
-      if (db?.auctions[url.name]) {
-        const { title, requirements } = db;
+  io.once("connection", socket => {
+    if (db?.auctions[url.name]) {
+      const { title, requirements } = db;
 
+      console.log(title, req.url);
+
+      socket.on("registration", () => {
         socket.emit(
           "registration",
           JSON.stringify({
@@ -42,40 +44,9 @@ app.get("/*", (req, res) => {
             },
           })
         );
-      }
-    });
-  }
-
-  if (/\/test/.test(req.url)) {
-    io.once("connection", socket => {
-      socket.emit("connection", state.count);
-      console.log("a user connected");
-
-      socket.on("connection", () => {
-        socket.emit("connection", { count: state.count, id: socket.id });
-        state.users.push(socket.id);
-        console.log(`a user: ${socket.id} connected`);
       });
-
-      socket.on("disconnecting", reason => {
-        for (const room of socket.rooms) {
-          if (room !== socket.id) {
-            socket.to(room).emit("left", socket.id);
-          }
-        }
-      });
-
-      socket.on("disconnect", () => {
-        console.log(`user ${socket.id} disconnected`);
-      });
-
-      socket.on("inc", count => {
-        state.count = count + 1;
-        io.emit("inc", count + 1);
-        console.log("inc", count);
-      });
-    });
-  }
+    }
+  });
 
   res.sendFile(join(__dirname, env.APP_URL, "index.html"));
 });

dd27c8a078c57b63d475dcb036294d85c2f7f040
Kirill Lavrov <lkv@myttk.ru>
2024-11-24 03:50:25 +0700

    fixed registration .10

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import express from "express";
 import { createServer } from "node:http";
 import { join, parse } from "node:path";
 import { Server } from "socket.io";
-import { title } from "node:process";
+import { emit, title } from "node:process";
 
 const state: {
   count: number;
@@ -24,13 +24,37 @@ app.get("/*", (req, res) => {
     return res.sendFile(join(__dirname, env.APP_URL, req.url));
   }
 
-  const db = JSON.parse(readFileSync(join(__dirname, env.DB_URL, "db.json"), "utf8"));
-  const url = parse(req.url);
+  if (/\/registration/.test(req.url)) {
+    const db = JSON.parse(readFileSync(join(__dirname, env.DB_URL, "db.json"), "utf8"));
+    const url = parse(req.url);
+
+    io.once("connection", socket => {
+      socket.emit("connection", socket.id);
+      console.log("connection registration", req.url);
+
+      if (db?.auctions[url.name]) {
+        const { title, requirements } = db?.auctions[url.name];
+
+        console.log(title, req.url);
+
+        socket.emit(
+          "registration",
+          JSON.stringify({
+            id: socket.id,
+            settings: {
+              title,
+              requirements,
+            },
+          })
+        );
+      }
+    });
+  }
 
   // if (/\/test/.test(req.url)) {
   //   io.once("connection", socket => {
   //     socket.emit("connection", state.count);
-  //     console.log("a user connected");
+  //     console.log(`a user ${socket.id} connected`);
 
   //     socket.on("connection", () => {
   //       socket.emit("connection", { count: state.count, id: socket.id });
@@ -57,33 +81,6 @@ app.get("/*", (req, res) => {
   //     });
   //   });
   // }
-
-  io.once("connection", socket => {
-    socket.emit("connection", socket.id);
-    // socket.on("connection", () => {
-    //   console.log(url.name, socket.id, socket.rooms);
-    // });
-
-    socket.on("registration", () => {
-      console.log(url.name);
-
-      if (db?.auctions[url.name]) {
-        const { title, requirements } = db?.auctions[url.name];
-        console.log(title, req.url);
-
-        socket.emit(
-          "registration",
-          JSON.stringify({
-            id: socket.id,
-            settings: {
-              title,
-              requirements,
-            },
-          })
-        );
-      }
-    });
-  });
 
   res.sendFile(join(__dirname, env.APP_URL, "index.html"));
 });
